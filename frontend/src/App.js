086252f5a2080472b5bff7b0d1f5baeaa4956ed5
Vaibhav Maheshwari (CW) <Vaibhav.Maheshwari@jda.com>
2024-03-12 15:24:23 +0530

    Login and register added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home.jsx';
+import Login from './components/Login.jsx';
+import Register from './components/Register.jsx';
+
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-      <header className="Content">
-        <h1>This is header.</h1>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
