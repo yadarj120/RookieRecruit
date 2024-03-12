0255a542c3170696a44e478013788612d4da1197
Arjun Yadav <yadarj120@gmail.com>
2024-03-12 16:52:00 +0530

    changed cicd

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,15 +4,15 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="social-media">
-        <a href="#"><i className="fab fa-facebook"></i></a>
-        <a href="#"><i className="fab fa-instagram"></i></a>
-        <a href="#"><i className="fab fa-twitter"></i></a>
+        <a href="/"><i className="fab fa-facebook"></i></a>
+        <a href="/"><i className="fab fa-instagram"></i></a>
+        <a href="/"><i className="fab fa-twitter"></i></a>
       </div>
       <div className="links">
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Services</a>
-        <a href="#">Contact</a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Services</a>
+        <a href="/">Contact</a>
       </div>
       <div className="copyright">
         <p>&copy; 2024 All rights reserved</p>
