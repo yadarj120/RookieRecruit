e016b0f71ddf9a02c1fa56a0972702539d615ace
Vaibhav Maheshwari (CW) <Vaibhav.Maheshwari@jda.com>
2024-03-13 14:33:52 +0530

    Job and user added

--- FSD/src/main/java/com/example/demo/repo/JobRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.example.demo.model.Job; // Assuming your Job entity class is in the com.example.demo.model package
-
-public interface JobRepo extends MongoRepository<Job, Integer> {
-
-}
