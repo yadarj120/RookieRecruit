788fbb7d4343235a860267e2f4e42056ad77ad16
Arjun Yadav <yadarj120@gmail.com>
2024-03-14 10:28:13 +0530

    removed spring security module and mongoService

--- backend/src/main/java/com/blueyonder/backend/controller/JobController.java
+++ backend/src/main/java/com/blueyonder/backend/controller/JobController.java
@@ -10,11 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 public class JobController {
 
     @Autowired
     private JobService service;
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Blue Yonder";
+    }
 
     @GetMapping("/getJob")
     public List<Job> fetchJobList() {
